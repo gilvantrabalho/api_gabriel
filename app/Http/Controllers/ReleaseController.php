e79f542d7dc044ff18aa67916cc2572cbbe122d7
gilvantrabalho <gilvan.trabalho07@gmail.com>
2022-06-07 13:17:04 -0300

    ...

--- app/Http/Controllers/ReleaseController.php
+++ app/Http/Controllers/ReleaseController.php
@@ -11,58 +11,46 @@ class ReleaseController extends Controller
     public function index()
     {
         return response()->json([
-            'error' => false,
-            'response' => [
-                'releases' => $this->releaseRepository->getAll(),
-            ]
+            'status' => true,
+            'releases' => Release::all(),
         ]);
     }
 
     public function store(Request $request)
     {
-        // id, user_id, transaction_id, operation_type, created_at
         try {
             $credentials = $request->only('user_id', 'transaction_id', 'operation_type');
             $validator = Validator::make($credentials, [
                 'user_id' => 'required',
                 'transaction_id' => 'required',
                 'operation_type' => 'required'
-            ], [
-                'user_id.required' => 'id_user é um campo obrigatório!',
-                'transaction_id.required' => 'transaction_id é um campo obrigatório',
-                'operation_type.required' => 'operation_type é um campo obrigatório'
             ]);
 
             if ($validator->fails()) {
                 return response()->json([
-                    'error' => true,
-                    'response' => [
-                        'message' => $validator->errors()
-                    ]
+                    'status' => false,
+                    'message' => $validator->errors()
                 ]);
             }
 
-            $dataArray = [
+            $release = new Release([
                 'user_id' => $request->user_id,
                 'transaction_id' => $request->transaction_id,
                 'operation_type' => $request->operation_type,
                 'created_at' => date('Y-m-d: H:i:s')
-            ];
+            ]);
+            $release->save();
 
-            if (!$this->releaseRepository->create($dataArray)) {
+            if (!$release) {
                 return response()->json([
-                    'error' => true,
-                    'response' => [
-                        'message' => 'Erro ao tentar cadastrar novo lançamento!'
-                    ]
+                    'status' => false,
+                    'message' => 'Erro ao tentar cadastrar novo lançamento!'
                 ]);
             }
 
             return response()->json([
-                'error' => false,
-                'response' => [
-                    'message' => 'Lançamento cadastrado com sucesso!'
-                ]
+                'status' => true,
+                'message' => 'Lançamento cadastrado com sucesso!'
             ]);
         } catch (\Exception $e) {
             return $e->getMessage();
--- app/Models/Release.php
+++ app/Models/Release.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Release extends Model
 {
+    public $table = "oliveira_gabriel.user_releases";
+    public $timestamps = false;
+
     use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'transaction_id', 'operation_type', 'created_at'
+    ];
 }
